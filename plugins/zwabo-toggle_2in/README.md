75f9ecc1afb7c2f81628d4355b5ac411cb21f560
falkTX <falktx@falktx.com>
2023-12-15 15:35:11 +0100

    Add new zwabo plugins
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ plugins/zwabo-toggle_2in/README.md
@@ -0,0 +1,20 @@
+# Toggle_2IN
+.lv2 plugin utility for Mod / Toggle switch
+
+Same as the MOD switchbox plugin but switch possible with CV
+
+A slight smoothing is implemented in order to avoid scale effects.
+
+----
+
+Mod devices installation
+
+- Copy the zwabo-toggle_2in.lv2 folder to your Mod:
+
+```
+scp -rp <path to zwabo-toggle_2in.lv2> root@192.168.51.1:/root/.lv2
+```
+
+- Enter password "mod"
+
+- Reboot Mod
---
+++ plugins/zwabo-xfade1to2st/DistrhoPluginInfo.h
@@ -0,0 +1,34 @@
+/*
+ * DPF Max Gen
+ * Copyright (C) 2015-2023 Filipe Coelho <falktx@falktx.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with
+ * or without fee is hereby granted, provided that the above copyright notice and this
+ * permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
+ * TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN
+ * NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#pragma once
+
+#define DISTRHO_PLUGIN_BRAND   "zwabo"
+#define DISTRHO_PLUGIN_NAME    "Xfade1to2st"
+#define DISTRHO_PLUGIN_URI     "urn:maxgen:zwabo:xfade1to2st"
+#define DISTRHO_PLUGIN_CLAP_ID "maxgen.zwabo.xfade1to2st"
+
+#define DISTRHO_PLUGIN_HAS_UI        0
+#define DISTRHO_PLUGIN_IS_RT_SAFE    1
+#define DISTRHO_PLUGIN_NUM_INPUTS    2
+#define DISTRHO_PLUGIN_NUM_OUTPUTS   4
+
+#define DISTRHO_PLUGIN_CLAP_FEATURES   "audio-effect", "routing", "stereo"
+#define DISTRHO_PLUGIN_LV2_CATEGORY    "lv2:UtilityPlugin"
+#define DISTRHO_PLUGIN_VST3_CATEGORIES "Fx|Utility"
+
+#define DISTRHO_PLUGIN_UNIQUE_ID d_cconst('z', 'w', 'x', 'f')
+#define DISTRHO_PLUGIN_VERSION d_version(0, 0, 0)
